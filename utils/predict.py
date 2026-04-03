8fbac542dc65c2f3678be771a6db9ecb5163ef0f
Ayush Rajput <ayush@gmail.com>
2026-04-04 00:13:00 +0530

    final fix: removed autoencoder completely

--- utils/predict.py
+++ utils/predict.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def predict(data, scaler, iso, autoencoder):
+def predict(data, scaler, iso, autoencoder=None):
     data_scaled = scaler.transform(data)
 
+    # Isolation Forest prediction
     iso_pred = iso.predict(data_scaled)
 
-    recon = autoencoder.predict(data_scaled)
-    loss = np.mean((data_scaled - recon) ** 2)
+    # Simple fraud score (NO tensorflow)
+    fraud_score = np.mean(np.abs(data_scaled))
 
-    return iso_pred, float(loss)
+    return iso_pred, float(fraud_score)
